5024c71a5cc436ece9293bbdd3302ddf1e41154b
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2026-04-11 19:18:21 -0300

    chore: initialize project structure

---
+++ build.sh
@@ -0,0 +1,1 @@
+gcc main.c -lncursesw -o main -Wall -Wextra
---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <ncurses.h>
+
+int main() {
+  initscr();
+  printw("Hello, World!\n");
+  refresh();
+  getch();
+  endwin();
+
+  return 0;
+}
