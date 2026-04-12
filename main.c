b6f114e538b704a9c89dba66a9f576f8076a5a9f
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2026-04-12 15:24:47 -0300

    feat: implement file saving to persist buffer content to disk

--- main.c
+++ main.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <ncurses.h>
 
 #define ctrl(x) ((x) & 0x1f)
 
-#define BACKSPACE 263
+#define BACKSPACE 127 
 #define ESCAPE 27
 
 typedef enum {
@@ -12,6 +13,8 @@ typedef enum {
 } Mode;
 
 Mode mode = NORMAL;
+
+int QUIT = 0;
 
 char* stringify_mode() {
   switch(mode) {
@@ -27,12 +30,15 @@ char* stringify_mode() {
   }
 }
 
-int main() {
+int main(void) {
   initscr();
   raw();
   keypad(stdscr, TRUE);
   noecho();
 
+  char *buf = malloc(sizeof(char) * 1024);
+  size_t buf_s = 0;
+  
   int row, col;
   getmaxyx(stdscr, row, col);
 
@@ -41,7 +47,8 @@ int main() {
 
   int ch = 0;
   int x = 0, y = 0;
-  while (ch != ctrl('q')) {
+  while (ch != ctrl('q') && QUIT != 1) {
+    refresh();
     mvprintw(row - 1, 0, stringify_mode());
     move(y, x);
     ch = getch();
@@ -49,6 +56,11 @@ int main() {
       case NORMAL:
         if (ch == 'i') {
           mode = INSERT;
+        } else if (ch == ctrl('s')) {
+            FILE *file = fopen("out.txt", "w");
+            fwrite(buf, buf_s, 1, file);
+            fclose(file);
+            QUIT = 1;
         }
         break;
       case INSERT:
@@ -57,10 +69,12 @@ int main() {
           getyx(stdscr, y, x);
           move(y, x - 1);
           delch();
+          buf[buf_s--] = ' ';
         } else if (ch == ESCAPE) {
           mode = NORMAL;
           keypad(stdscr, TRUE);
         } else {
+          buf[buf_s++] = ch;
           addch(ch);
         }
         break;
